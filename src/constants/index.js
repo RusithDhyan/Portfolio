4573368f822218e00158dbcfe4a1c01f3ddfd382
RusithDhyan <rusithdhyan2000@gmail.com>
2024-07-15 14:35:26 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,217 @@
+import { meta  } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    
+   
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+   
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: nextjs,
+    //     name: "Next.js",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: redux,
+    //     name: "Redux",
+    //     type: "State Management",
+    // },
+    // {
+    //     imageUrl: sass,
+    //     name: "Sass",
+    //     type: "Frontend",
+    // },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+   
+   
+  
+];
+
+export const experiences = [
+    {
+        title: "React.js & Java Developer (Intern)",
+        company_name: "Sri Lanka Police",
+        icon: react,
+        iconBg: "#accbe1",
+        date: "Jan 2024 - July 2024",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    
+    {
+        title: "Full Stack Developer(Group Project II)",
+        company_name: "Cuddle Care",
+        icon: nodejs,
+        iconBg: "#b7e4c7",
+        date: "May 2023 - Dec 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer(Group Project I)",
+        company_name: "Up Keep",
+        icon: html,
+        iconBg: "#a2d2ff",
+        date: "May 2022 - Feb 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/RusithDhyan',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/rusith-dhyan-19b02424b',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Complaint Management System',
+        description: 'Developed an Incident Management System for the Sr Lanka Police aimed at managing complaints from complainants and police officers, providing higher-ranking officers with a better option to follow up on the progress of each case.',
+        link: 'https://github.com/SL-Police-ITD/Incident-management-system/tree/core_main',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Baby Care System',
+        description: 'Created a full-stack project for babysitting system ',
+        link: 'https://github.com/Sewmi1216/Baby-Care-System',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'House Hold Management System',
+        description: 'Designed and built a Web App for maintaining house hold equipment',
+        link: 'https://github.com/nipunar99/UPKEEP_2nd-Year-GroupProject_CS40',
+    },
+    // {
+    //     iconUrl: snapgram,
+    //     theme: 'btn-back-pink',
+    //     name: 'Full Stack Instagram Clone',
+    //     description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+    //     link: 'https://github.com/adrianhajdin/social_media_app',
+    // },
+    // {
+    //     iconUrl: estate,
+    //     theme: 'btn-back-black',
+    //     name: 'Real-Estate Application',
+    //     description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+    //     link: 'https://github.com/adrianhajdin/projects_realestate',
+    // },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Cake Selling Site',
+        description: 'Created a simple site for selling our own cakes through a Ecwid site',
+        link: 'https://sweetcakeslk.company.site/',
+    }
+];
